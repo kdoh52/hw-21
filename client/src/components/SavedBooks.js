29cec152eaa49d2abe5f2445e678835a870be225
kdoh52 <kdoh52@gmail.com>
2020-11-29 20:09:45 -0800

    saved books display

--- client/src/components/SavedBooks.js
+++ client/src/components/SavedBooks.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import API from "../utils/API";
+import Book from "./Book";
+
 
 const useStyles = makeStyles(theme => ({
     title: {
@@ -10,14 +13,41 @@ const useStyles = makeStyles(theme => ({
 }))
 
 
-export default function SavedBooks() {
+function SavedBooks() {
     let classes = useStyles();
+    const [books, setBooks] = useState([])
+
+
+    useEffect(() => {
+        searchDatabase()
+    }, [])
+
+    function searchDatabase() {
+        API.getBooks()
+        .then(res => 
+            setBooks(res.data)
+        ).catch(err => console.log(err));
+    }
+    
 
     return (
         <div>
             <Typography className={classes.title} variant='h4'>
                 Saved Books
             </Typography>
+            {console.log(books)}
+            {books.map(book => (
+                <Book 
+                    key={book._id}
+                    title={book.title}
+                    author={book.authors}
+                    description={book.description}
+                    image={book.image}
+                    link={book.link}
+                />
+            ))}
         </div>
     )
 }
+
+export default SavedBooks;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
 export default {
-
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
     saveBook: function(bookData) {
         console.log("THIS IS BOOK DATA", bookData)
         return axios.post("/api/books", bookData);
+    },
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
     }
 //   getUsers: function() {
 //     return axios.get("/api/users");
